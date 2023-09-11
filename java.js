1cb946957a579af90e6e4bea7a8f7b2dcb31ebbf
java12346 <olusanya4real@gmail.com>
2023-09-11 09:42:40 +0100

    updated javascript

--- java.js
+++ java.js
@@ -1,5 +1,21 @@
-setInterval(() => {
-  const utcTimeElement = document.getElementById('utcTime');
-  const currentUTCTime = new Date().getTime();
-  utcTimeElement.textContent = currentUTCTime;
-}, 1000);
+function updateDayOfTheWeek() {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const now = new Date();
+  
+  const dayOfWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+  dayOfWeekElement.textContent = daysOfWeek[now.getUTCDay()];
+
+  
+}
+
+function updateUTCTime() {
+  let currentUTCTime = Date.now();
+  let UTCTimeEelement = document.querySelector('[data-testid="currentUTCTime"]');
+  UTCTimeEelement.textContent = currentUTCTime;
+}
+
+  updateDayOfTheWeek();
+  updateUTCTime();
+
+  setInterval(updateDayOfTheWeek, 1000);
+  setInterval(updateUTCTime, 1000);
